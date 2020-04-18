7c6a09c5bc1d266f74e24669caf6fb01a8618c95
Mg.raaf <59801203+rafik356@users.noreply.github.com>
2020-04-18 21:45:33 +0100

    Add files via upload

---
+++ home.php
@@ -0,0 +1,23 @@
+<?php 
+//session_star();
+//connect to database
+
+
+?>
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Register</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="header">
+		<h1>register</h1>
+	</div>
+	<h1>Home</h1>
+	<div><h4>Welcome <?php echo $_SESSION['username']; ?></h4></div>
+	
+</body>
+</html>
