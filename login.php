3e6d6be0bd17c556360c5ab9a6d6ab22060a3ecf
Mg.raaf <59801203+rafik356@users.noreply.github.com>
2020-04-18 04:37:08 +0100

    Add files via upload

---
+++ login.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>login Page</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div id="frm">
+	<form action="process.php" method="POST">
+		<p>
+			<label>Username:</label>
+			<input type="text" id="username" name="username" />
+		</p>
+		<p>
+			<label>Password</label>
+			<input type="text" id="password" name="password" />
+		</p>
+		<p>
+			
+			<input type="submit" id="btn" value ="Login" />
+		</p>
+	</form>
+</div>
+</body>
+</html>
