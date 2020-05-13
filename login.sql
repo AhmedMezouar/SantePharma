eae67b177667b0685e8c8681224de496fbb3be8a
AhmedMezouar <46110851+AhmedMezouar@users.noreply.github.com>
2020-05-13 11:53:56 +0100

    Update login.sql

--- login.sql
+++ login.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1
--- Généré le : mer. 13 mai 2020 à 02:41
+-- Généré le : mer. 13 mai 2020 à 12:52
 -- Version du serveur :  10.4.11-MariaDB
 -- Version de PHP : 7.4.4
 
@@ -36,13 +36,28 @@ CREATE TABLE `medicament` (
 -- --------------------------------------------------------
 
 --
--- Structure de la table `offre_travail`
+-- Structure de la table `postuler`
 --
 
-CREATE TABLE `offre_travail` (
-  `id_de_off` varchar(25) NOT NULL,
-  `nom_de_off` varchar(25) NOT NULL
+CREATE TABLE `postuler` (
+  `id` int(10) NOT NULL,
+  `nom` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `phone` varchar(15) NOT NULL,
+  `dateN` date NOT NULL,
+  `specialiste` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Déchargement des données de la table `postuler`
+--
+
+INSERT INTO `postuler` (`id`, `nom`, `email`, `phone`, `dateN`, `specialiste`) VALUES
+(1, '', 'ahmed.mezouar26@gmail.com', '+213671205465', '0000-00-00', ''),
+(2, 'ccc', 'CCCC@gmail.com', '12345', '2020-05-08', 'infirmier'),
+(3, 'ccc', 'CCCC@gmail.com', '12345', '2020-05-08', 'infirmier'),
+(4, 'bbb', 'BBB@gmail.com', '12345', '2020-05-01', 'veneur_Pharmacie'),
+(5, 'eee', 'eee@gmail.com', '+213671205465', '2020-05-10', 'infirmier');
 
 -- --------------------------------------------------------
 
@@ -51,7 +66,7 @@ CREATE TABLE `offre_travail` (
 --
 
 CREATE TABLE `user` (
-  `id` int(11) NOT NULL,
+  `id_U` int(11) NOT NULL,
   `Nom` varchar(20) NOT NULL,
   `prenom` varchar(20) NOT NULL,
   `date` date NOT NULL,
@@ -63,15 +78,15 @@ CREATE TABLE `user` (
   `HOF` varchar(6) NOT NULL,
   `numAgre` varchar(20) NOT NULL,
   `numTel` varchar(20) NOT NULL,
-  `dateInsec` date NOT NULL DEFAULT current_timestamp()
+  `dateInsec` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Déchargement des données de la table `user`
 --
 
-INSERT INTO `user` (`id`, `Nom`, `prenom`, `date`, `email`, `password`, `Sexe`, `specialte`, `HOV`, `HOF`, `numAgre`, `numTel`, `dateInsec`) VALUES
-(1, 'Mezouar', 'ahmed', '1998-09-26', 'ahmed.mezouar26@gmail.com', '12345', 'homme', 'Medecin', '08:00', '20:00', '25252525', '+213671205465', '2020-05-13');
+INSERT INTO `user` (`id_U`, `Nom`, `prenom`, `date`, `email`, `password`, `Sexe`, `specialte`, `HOV`, `HOF`, `numAgre`, `numTel`, `dateInsec`) VALUES
+(1, 'Mezouar', 'ahmed', '1998-09-26', 'ahmed.mezouar26@gmail.com', '12345', 'homme', 'Medecin', '08:00', '20:00', '25252525', '+213671205465', '2020-05-12 23:00:00');
 
 --
 -- Index pour les tables déchargées
@@ -84,26 +99,32 @@ ALTER TABLE `medicament`
   ADD PRIMARY KEY (`ref_medic`);
 
 --
--- Index pour la table `offre_travail`
+-- Index pour la table `postuler`
 --
-ALTER TABLE `offre_travail`
-  ADD PRIMARY KEY (`id_de_off`);
+ALTER TABLE `postuler`
+  ADD PRIMARY KEY (`id`);
 
 --
 -- Index pour la table `user`
 --
 ALTER TABLE `user`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id_U`);
 
 --
 -- AUTO_INCREMENT pour les tables déchargées
 --
 
 --
+-- AUTO_INCREMENT pour la table `postuler`
+--
+ALTER TABLE `postuler`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id_U` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
